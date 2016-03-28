18c52f1b0f5bcb5da1b4f422b9f0523c24c028de
jspring <jspring@path.berkeley.edu>
2016-03-28 15:55:40 -0700

    modified:   clt_vars.c Changed db_vars_list to db_controller_list

--- src/clt_vars.c
+++ src/clt_vars.c
@@ -65,14 +65,14 @@ int main(int argc, char *argv[])
 	 */
 	get_local_name(hostname, MAXHOSTNAMELEN);
 	if ((pclt = db_list_init(argv[0], hostname, domain, xport, 
-			db_vars_list, num_db_vars, NULL, 0)) == NULL) {
+			db_controller_list, num_controller_vars, NULL, 0)) == NULL) {
 		printf("Database initialization error in %s.\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	for (i = 0; i < num_db_vars; i++){
+	for (i = 0; i < num_controller_vars; i++){
 		db_clt_write(pclt,
-		db_vars_list[i].id,
-		db_vars_list[i].size,
+		db_controller_list[i].id,
+		db_controller_list[i].size,
 		zero_array);
 	}
         
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
 	}
 
 	if(( exitsig = setjmp(exit_env)) != 0) {
-		db_list_done(pclt, db_vars_list, num_db_vars, NULL, 0);
+		db_list_done(pclt, db_controller_list, num_controller_vars, NULL, 0);
 		exit(EXIT_SUCCESS);
 	} else
 		sig_ign(sig_list, sig_hand);
