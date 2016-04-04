ffe22c0a7cc2a1f489cb7b77352c9008641fab24
jspring <jspring@path.berkeley.edu>
2016-04-04 14:44:24 -0700

    new file:   ../src/rm_table.h Has extern references to stuff in rm_table.c
    new file:   replay_controller_go
    Script for running replay of a data file
    new file:   replay_controller_go_test
    
    new file:   replay_controller_set
    Draft of a script for replaying a set of data files. I haven't decided the
    best way yet.
    
    new file:   replay_controller_go_test
    Temp script that works with the data in sr99_ramp_metering/test/data
    new file:   restart_system
    Script for checking whether urms and wrfiles_rm are running for all
    the controllers, and attempting to restart them if they are not.
    new file:   stopreplay
    Stops replay system from running

---
+++ src/rm_table.h
@@ -0,0 +1,11 @@
+#include <data_log.h>
+#include <urms.h>
+
+extern data_log_column_spec_t file_spec[];
+extern int num_file_columns;
+
+extern db_var_spec_t db_vars_ac_rm[];
+extern int num_ac_rm_vars;
+
+extern db_urms_status_t db_urms_status;
+extern timestamp_t timestamp;
