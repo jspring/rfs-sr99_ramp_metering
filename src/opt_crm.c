4c2199796108cdddbe26114e2cb2d374c8e5de7d
jspring <jspring@path.berkeley.edu>
2016-04-05 11:53:02 -0700

    modified:   opt_crm.c XYL modified printfs to save Cheng-Ju's output

--- src/opt_crm.c
+++ src/opt_crm.c
@@ -150,8 +150,8 @@ for(int i=0; i<SecSize;i++){
 			    detection_s[i]->data[Np-1].occupancy=Mind(100.0, Maxd(100.0*(mainline_out[i].agg_occ), 5.0*(1.0+0.5*rand()/RAND_MAX)));
 			    detection_s[i]->data[Np-1].density=Mind(1200.0, Maxd(mainline_out[i].agg_density, 10.0*(1.0+0.5*rand()/RAND_MAX)));  
 			    
-			    fprintf(st_file_out,"%f %f %f %f ", detection_s[i]->data[Np-1].flow, detection_s[i]->data[Np-1].speed, 
-			                                           detection_s[i]->data[Np-1].occupancy, detection_s[i]->data[Np-1].density); 
+			    fprintf(st_file_out,"%f %f %f %f ", mainline_out[i].agg_vol, mainline_out[i].agg_speed, 
+			                                           mainline_out[i].agg_occ, mainline_out[i].agg_density); 
 		}			                               
 	
 		
@@ -161,8 +161,8 @@ for(int i=0; i<SecSize;i++){
 				detection_onramp[i]->data[Np-1].occupancy=Mind(100.0, Maxd(100.0*(onramp_out[i].agg_occ), 5.0*(1.0+0.5*rand()/RAND_MAX))); 
 				detection_offramp[i]->data[Np-1].flow=Mind(12000.0, Maxd(offramp_out[i].agg_vol, 100.0*(1.0+0.5*rand()/RAND_MAX)));
 				detection_offramp[i]->data[Np-1].occupancy=Mind(100.0, Maxd(100.0*(offramp_out[i].agg_occ), 5.0*(1.0+0.5*rand()/RAND_MAX))); 	
-				fprintf(st_file_out,"%f %f %f %f ", detection_onramp[i]->data[Np-1].flow, detection_onramp[i]->data[Np-1].occupancy,
-														detection_offramp[i]->data[Np-1].flow, detection_offramp[i]->data[Np-1].occupancy);  			
+				fprintf(st_file_out,"%f %f %f %f ", onramp_out[i].agg_vol, 100.0*(onramp_out[i].agg_occ,
+														offramp_out[i].agg_vol, offramp_out[i].agg_occ);  			
 		}
 		fprintf(st_file_out,"\n");
 		
