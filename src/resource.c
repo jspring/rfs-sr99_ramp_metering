b881b032ef2b07e30cdd03833d40bda8d2babc65
chengju <bigfindings@gmail.com>
2016-04-13 16:26:33 -0700

    modified:   resource.c replace NaN by -1.

--- src/resource.c
+++ src/resource.c
@@ -72,7 +72,7 @@ float flow_aggregation_mainline(db_urms_status_t *controller_data){
 		}
 	}
 	if(flow != flow ){
-		flow = 0;
+		flow = -1;
 	}
 	printf("ML-flow_agg %4.2f num_main %d\n", flow, controller_data->num_main);
 	return flow;
@@ -95,7 +95,7 @@ float flow_aggregation_onramp(db_urms_status_t *controller_data){
 			}
 	}
 	if(flow != flow ){
-		flow = 0;
+		flow = -1;
 	}
 	printf("OR-flow_agg %4.2f num_meter %d\n",	flow, controller_data->num_meter);
 	return flow;
@@ -118,7 +118,7 @@ float flow_aggregation_offramp(db_urms_status_t *controller_data){
 		}
 	}
 	if(flow != flow ){
-		flow = 0;
+		flow = -1;
 	}
 	printf("FR-flow_agg %4.2f num_addl_det %d\n", flow, controller_data->num_addl_det );
 	return flow;
@@ -144,7 +144,7 @@ float occupancy_aggregation_mainline(db_urms_status_t *controller_data){
 	occupancy /= controller_data->num_main;
     // check Nan 
 	if(occupancy != occupancy){
-		occupancy = 0;
+		occupancy = -1;
 	}
 	printf("Occ_agg %4.2f num_main %d\n", occupancy, controller_data->num_main);
 	return occupancy;
@@ -171,7 +171,7 @@ float speed_aggregation_mainline(db_urms_status_t *controller_data){
 	speed = (controller_data->num_main)/tmp;
 	// check Nan 
 	if(speed != speed){
-		speed = 0;
+		speed = -1;
 	}
 	printf("speed_agg %4.2f num_main %d\n", speed, controller_data->num_main);
 	return speed;
@@ -197,7 +197,7 @@ float mean_speed_aggregation_mainline(db_urms_status_t *controller_data){
 	speed /= controller_data->num_main;
 	// check Nan 
 	if(speed != speed){
-		speed = 0;
+		speed = -1;
 	}
 	printf("mean_speed_agg %4.2f num_main %d\n", speed,	controller_data->num_main);
 	return speed;
@@ -236,7 +236,7 @@ float queue_onramp(db_urms_status_t *controller_data){
 	}
 	// check Nan 
 	if(queue != queue){
-		queue = 0;
+		queue = -1;
 	}
 	printf("queue_agg %4.2f num_meter %d\n", queue,controller_data->num_meter);
 	return queue;
@@ -269,7 +269,13 @@ float density_aggregation_mainline(db_urms_status_t *controller_data){
 	}
 	// check Nan 
 	if(density != density){
-		density = 0;
+		density = -1;
+	}
+	if(flow != flow){
+		flow = -1;
+	}
+	if(speed != speed){
+		speed = -1;
 	}
      printf("flow %4.2f speed %4.2f density_agg %4.2f \n",
 			flow,
