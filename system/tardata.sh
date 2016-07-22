19f674895b7ddb9f656fd2f8bede8a10edd024dc
jspring <jspring@path.berkeley.edu>
2016-07-21 22:29:17 -0700

    modified:   tardata.sh Added scp of tarred data to pems server

--- system/tardata.sh
+++ system/tardata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 TIMESTAMP=`ls -tr /big/data/ac_rm_1 | tail -1| sed '{s/\./ /g}'|awk '{print $(NF-1)}'`
+cd /big/data/ac_rm_1
 tar cvzf /big/data/"ac_rm_"$TIMESTAMP".tgz" /big/data/ac_rm_1/* --exclude "*$TIMESTAMP*"
-#cd /big/data/ac_rm_1
+scp /big/data/"ac_rm_"$TIMESTAMP".tgz" caltrans@128.32.234.154:.
 #for x in `ls | grep -v $TIMESTAMP` 
 #do 
 #	echo $x
