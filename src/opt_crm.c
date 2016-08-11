95121cdcec58642446fc820cfd9918dc3de5f12f
chengju <bigfindings@gmail.com>
2016-08-11 13:10:00 -0700

    modified:   look_up_table.h modified:   opt_crm.c modified:   resource.h

--- src/opt_crm.c
+++ src/opt_crm.c
@@ -413,7 +413,7 @@ int j; //
 
 	  if(mean_array(temp_ary_OR_vol,NUM_CYCLE_BUFFS)==0.0){
 		  if(OR_flow_zero_counter[i] > num_zero_tolerant ){
-                  onramp_out_f[i].agg_vol = interp_OR_HIS_FLOW(i+6, OR_HIS_FLOW_DATA);
+                  onramp_out_f[i].agg_vol = interp_OR_HIS_FLOW(i+1+5, OR_HIS_FLOW_DATA);
 		  }else{
 		          onramp_out_f[i].agg_vol = mean_array(temp_ary_OR_vol,NUM_CYCLE_BUFFS);
 		  }
@@ -425,7 +425,7 @@ int j; //
 
 	  if(mean_array(temp_ary_OR_occ,NUM_CYCLE_BUFFS)==0.0){
 		  if(OR_occ_zero_counter[i] > num_zero_tolerant ){
-                  onramp_out_f[i].agg_occ = interp_OR_HIS_OCC(i+6, OR_HIS_OCC_DATA);
+                  onramp_out_f[i].agg_occ = interp_OR_HIS_OCC(i+1+5, OR_HIS_OCC_DATA);
 		  }else{
 		          onramp_out_f[i].agg_occ = mean_array(temp_ary_OR_occ,NUM_CYCLE_BUFFS);
 		  }
@@ -438,7 +438,7 @@ int j; //
       
 	  if(mean_array(temp_ary_FR_vol,NUM_CYCLE_BUFFS)==0.0){
 		  if(FR_flow_zero_counter[i] > num_zero_tolerant ){
-                  offramp_out_f[i].agg_vol = interp_FR_HIS_FLOW(i, FR_HIS_FLOW_DATA);
+                  offramp_out_f[i].agg_vol = interp_FR_HIS_FLOW(i+1, FR_HIS_FLOW_DATA);
 		  }else{
 		          offramp_out_f[i].agg_vol = mean_array(temp_ary_FR_vol,NUM_CYCLE_BUFFS);
 		  }
@@ -450,7 +450,7 @@ int j; //
 
 	  if(mean_array(temp_ary_FR_occ,NUM_CYCLE_BUFFS)==0.0){
 		  if(FR_occ_zero_counter[i] > num_zero_tolerant ){
-                  offramp_out_f[i].agg_occ = interp_FR_HIS_OCC(i, FR_HIS_OCC_DATA);
+                  offramp_out_f[i].agg_occ = interp_FR_HIS_OCC(i+1, FR_HIS_OCC_DATA);
 		  }else{
 		          offramp_out_f[i].agg_occ = mean_array(temp_ary_FR_occ,NUM_CYCLE_BUFFS);
 		  }
