8e67c2774022c3bcd523a560a0f76ebd567168de
John Spring <jspring@berkeley.edu>
2016-09-13 18:53:45 -0700

    new file:   mainline_occupancy.sh Grabs mainline occupancy data from data files

---
+++ system/mainline_occupancy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+A_FILE=$1
+
+if [[ q$A_FILE == 'q' ]]
+then
+	echo
+	echo "Usage: $0 <one filename in set>"
+	echo "Prints out the number of changes (i.e. number of different lines) in flow"
+	echo "for demand and passage detectors for all the lanes on all the controllers:"
+	echo "Column 1: number of different lines in the data file"
+	echo "Column 2: total number of lines in data file"
+	echo "Column 3: file name"
+	echo
+
+	exit 1
+fi
+
+MATCHSTR=`echo $A_FILE | sed '{s/\./ /g}' | awk '{print $(NF-1)}'`
+MYPATH=`echo $A_FILE | sed '{s/a_10/ &/g}'| awk '{print $1}'`
+
+echo $MATCHSTR
+
+for THIS_PATH in `ls $MYPATH*$MATCHSTR.dat`
+do
+	echo `cat $THIS_PATH | awk '{print $4,$7,$11,$14,$18,$21,$25,$28,$32,$35,$39,$42,$234,$237,$241,$244}' | uniq -c | wc | awk '{print $1}'` `wc $THIS_PATH | awk '{print $1,$4}'` 
+done
