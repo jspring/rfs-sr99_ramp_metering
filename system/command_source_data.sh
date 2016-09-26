2695a74a4fd7685acb990388b065ecc1c4268beb
John Spring <jspring@berkeley.edu>
2016-09-26 13:04:14 -0700

    new file:   command_source_data.sh new file:   metered_ips.txt new file:   start_xterm.sh New scripts for checking command source in data files and starting up X terminals for all controllers

---
+++ system/command_source_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+A_FILE=$1
+
+if [[ q$A_FILE == 'q' ]]
+then
+	A_FILE=`ls -t /big/data/ac_rm_1/a_10* | head  -1`
+fi
+
+MATCHSTR=`echo $A_FILE | sed '{s/\./ /g}' | awk '{print $(NF-1)}'`
+MYPATH=`echo $A_FILE | sed '{s/a_10/ &/g}'| awk '{print $1}'`
+
+echo $MATCHSTR
+
+for THIS_PATH in `ls $MYPATH*$MATCHSTR.dat`
+do
+#	echo `cat $THIS_PATH | awk '{print $57,$65,$251}' | uniq -c | wc | awk '{print $1}'` `wc $THIS_PATH | awk '{print $1,$4}'` 
+	echo `cat $THIS_PATH | awk '{print $57,$65,$251}' | uniq -c` `wc $THIS_PATH | awk '{print $1,$4}'` 
+done
