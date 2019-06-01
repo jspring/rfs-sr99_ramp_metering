24698c8b6248e9cb4ef35de4bb1ab24acab14e37
John Spring <jspring@berkeley.edu>
2019-06-01 15:15:15 -0700

    modified:   tardata.sh Changed prefix to logfile to sr99_ramp_metering to distinguish it from the San Jose corridor Changed destination of tarfiles from my public_html directory to /linux2/big/data/ac_rm_1rm_1

--- system/tardata.sh
+++ system/tardata.sh
@@ -14,14 +14,18 @@ do
 	x=$(($x+1))
 done
 
-FILENAME="ramp_coord_data_"$FILEDATE"_"$x".tgz"
+FILENAME="sr99_ramp_coord_data_"$FILEDATE"_"$x".tgz"
 OUT_DATA_DIRS=`ls -d /home/sr99_ramp_metering/src/Out_Data_$FILEDATE*`
 TIMESTAMP=`ls -tr /big/data/ac_rm_1/*dat | tail -1| sed '{s/\./ /g}'|awk '{print $(NF-1)}'`
 
 echo FILENAME $FILENAME OUT_DATA_DIRS $OUT_DATA_DIRS
 cd /big/data/ac_rm_1
 tar cvzf $FILENAME * $OUT_DATA_DIRS --exclude "*$TIMESTAMP*"
-scp $FILENAME jspring@128.32.234.154:public_html
+if [[ $? == 0 ]]
+then
+	echo tar OK
+fi
+scp $FILENAME 128.32.234.154:/linux2/big/data/ac_rm_1
 mv  *tgz /big/data/olddata
 for x in `ls | grep -v $TIMESTAMP` 
 do 
